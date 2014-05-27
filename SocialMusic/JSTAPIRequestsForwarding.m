e9ba297a83e2df040e5aa21d4dfeeaaa3cf01ad6
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-05-27 09:31:03 -0700

    no proxy new branch

--- SocialMusic/JSTAPIRequestsForwarding.m
+++ SocialMusic/JSTAPIRequestsForwarding.m
@@ -1,51 +1,51 @@
+////
+////  JSTAPIRequestsForwarding.m
+////  SocialMusic
+////
+////  Created by Javier Soto on 5/17/14.
+////  Copyright (c) 2014 Javier Soto. All rights reserved.
+////
 //
-//  JSTAPIRequestsForwarding.m
-//  SocialMusic
+//#import <JSTAPIToolsURLProtocol/JSTAPIToolsURLProtocol.h>
 //
-//  Created by Javier Soto on 5/17/14.
-//  Copyright (c) 2014 Javier Soto. All rights reserved.
+//// APItools username: apitools+test@javisoto.es
+//// APItools password: SocialMusic
 //
-
-#import <JSTAPIToolsURLProtocol/JSTAPIToolsURLProtocol.h>
-
-// APItools username: apitools+test@javisoto.es
-// APItools password: SocialMusic
-
-@interface JSTAPIRequestsForwarding : NSObject <JSTAPIToolsURLMapping>
-
-@end
-
-@implementation JSTAPIRequestsForwarding
-
-+ (void)load {
-  [JSTAPIToolsURLProtocol registerURLProtocolWithURLMapping:[self sharedInstance]];
-}
-
-+ (instancetype)sharedInstance {
-  static JSTAPIRequestsForwarding *requestForwarding = nil;
-
-  static dispatch_once_t onceToken;
-  dispatch_once(&onceToken, ^{
-    requestForwarding = [[self alloc] init];
-  });
-
-  return requestForwarding;
-}
-
-#pragma mark - JSTAPIToolsURLMapping
-
-- (NSString *)apiToolsHostForOriginalURLHost:(NSString *)originalURLHost {
-  static NSDictionary *mappingDictionary = nil;
-
-  static dispatch_once_t onceToken;
-  dispatch_once(&onceToken, ^{
-    mappingDictionary = @{
-                          @"api.musicgraph.com" : @"musicgraph-083ba6881b1c.my.apitools.com",
-                          @"api.twitter.com" : @"twitter-083ba6881b1c.my.apitools.com",
-                          };
-  });
-
-  return mappingDictionary[originalURLHost];
-}
-
-@end
+//@interface JSTAPIRequestsForwarding : NSObject <JSTAPIToolsURLMapping>
+//
+//@end
+//
+//@implementation JSTAPIRequestsForwarding
+//
+//+ (void)load {
+//  [JSTAPIToolsURLProtocol registerURLProtocolWithURLMapping:[self sharedInstance]];
+//}
+//
+//+ (instancetype)sharedInstance {
+//  static JSTAPIRequestsForwarding *requestForwarding = nil;
+//
+//  static dispatch_once_t onceToken;
+//  dispatch_once(&onceToken, ^{
+//    requestForwarding = [[self alloc] init];
+//  });
+//
+//  return requestForwarding;
+//}
+//
+//#pragma mark - JSTAPIToolsURLMapping
+//
+//- (NSString *)apiToolsHostForOriginalURLHost:(NSString *)originalURLHost {
+//  static NSDictionary *mappingDictionary = nil;
+//
+//  static dispatch_once_t onceToken;
+//  dispatch_once(&onceToken, ^{
+//    mappingDictionary = @{
+//                          @"api.musicgraph.com" : @"musicgraph-083ba6881b1c.my.apitools.com",
+//                          @"api.twitter.com" : @"twitter-083ba6881b1c.my.apitools.com",
+//                          };
+//  });
+//
+//  return mappingDictionary[originalURLHost];
+//}
+//
+//@end
